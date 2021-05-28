588c12475f3c38e6f1901d3519133a853a240e0c
Minwoo Kang <minukang5874@gmail.com>
2021-05-29 02:05:39 +0900

    💄 Update footer

--- js/components/Page.mjs
+++ js/components/Page.mjs
@@ -1,11 +1,17 @@
+import Logo from './Logo.mjs';
+
 export default Vue.defineComponent({
   name: 'page',
+  components: { Logo },
   template: `
   <article class="app">
     <header class="appHeader">
       <slot name="header" />
     </header>
     <slot />
+    <footer class="appFooter">
+      <Logo class="appFooter__logo" color="#d1d1d6" />
+    </footer>
   </article>
   `
 });
