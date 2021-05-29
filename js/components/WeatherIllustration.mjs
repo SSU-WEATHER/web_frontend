c47814740b2b78f6e3dde714a98b53e6c52dcd5b
Minwoo Kang <minukang5874@gmail.com>
2021-05-29 23:18:34 +0900

    ✨ Weather illustraion work

--- js/components/WeatherIllustration.mjs
+++ js/components/WeatherIllustration.mjs
@@ -1,7 +1,14 @@
 export default Vue.defineComponent({
   name: 'weather-illustration',
   props: ['weatherType'],
+  computed: {
+    weatherBackground() {
+      return {
+        backgroundImage: `url('/images/illustration/${this.weatherType}.png')`
+      };
+    }
+  },
   template: `
-    <figure class="weatherIllustration"></figure>
+    <figure class="weatherIllustration" :style="weatherBackground" />
   `
 });
